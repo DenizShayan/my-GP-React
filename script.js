725d22b840588e35f4f96c6ba9062d0b8b1dd881
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-07-19 13:11:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,11 @@ const resetBtn = document.getElementById("resetBtn");
 let moved = false;
 
 moveBtn.addEventListener("click", () => {
-  if (moved) {
-    cube.style.left = "0px";
-  } else {
-    cube.style.left = "200px";
-  }
+  cube.style.left = moved ? "0px" : "40px";
   moved = !moved;
 });
 
-resetBtn.addEventListener("click", () =>{
-  cube.style.left="0px";
-  moved= false;
+resetBtn.addEventListener("click", () => {
+  cube.style.left = "0px";
+  moved = false;
 });
