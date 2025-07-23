3b45edb094c4198c47dc1065744f5129a31d6c25
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-07-23 23:56:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const cube = document.getElementById("cube");
-const moveBtn = document.getElementById("moveBtn");
+const box = document.getElementById("box");
+const startBtn = document.getElementById("startBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-let moved = false;
+let posX = 0;
+let posY = 0;
+let velocityX = 2;
+let velocityY = 2;
+let animation;
 
-moveBtn.addEventListener("click", () => {
-  cube.style.left = moved ? "0px" : "40px";
-  moved = !moved;
+function moveBox() {
+  const container = document.querySelector(".container");
+  const maxX = container.clientWidth - box.clientWidth;
+  const maxY = container.clientHeight - box.clientHeight;
+
+  posX += velocityX;
+  posY += velocityY;
+
+  if (posX <= 0 || posX >= maxX) velocityX *= -1;
+  if (posY <= 0 || posY >= maxY) velocityY *= -1;
+
+  box.style.left = `${posX}px`;
+  box.style.top = `${posY}px`;
+
+  animation = requestAnimationFrame(moveBox);
+}
+
+startBtn.addEventListener("click", () => {
+  if (!animation) {
+    moveBox();
+  }
 });
 
 resetBtn.addEventListener("click", () => {
-  cube.style.left = "0px";
-  moved = false;
+  cancelAnimationFrame(animation);
+  animation = null;
+  posX = 0;
+  posY = 0;
+  box.style.left = "0px";
+  box.style.top = "0px";
 });
